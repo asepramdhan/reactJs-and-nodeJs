2b1e0a9d150ad504b44c053a24fd55796170775c
Your Name <asiepramdhan@gmail.com>
2021-05-21 10:02:07 +0700

    menampilkan hello world ke layar browser

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const db = require("./models");
 
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log(`Server started on 3001`);
